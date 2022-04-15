c55637d3ea3ed1ab130829e546c7be10d1350f39
Zdravko Zdravkov <zdr0@yahoo.com>
2022-04-15 14:32:42 +0200

    Exam

---
+++ EXAM/Variant 2/webapp/index.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <title>Submitted Items Statistics</title>
+    </head>
+    <body>
+        <h3>Submitted Items Statistics</h3>
+        <br />
+        <table border="1">
+			<tr><td>Item</td><td>Count</td></tr>
+			
+<?php
+	// CONNECTION INFORMATION START 
+	
+	// SQL Server Extension Sample Code:
+	$connectionInfo = array("UID" => "examsa", "pwd" => "ExamPassword2022", "Database" => "sqldb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+	$serverName = "tcp:zrzsqlsrv.database.windows.net,1433";
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+	// CONNECTION INFORMATION END
+
+	if( $conn === false ) {
+	     die( print_r( sqlsrv_errors(), true));
+	}
+
+	$stmt = sqlsrv_query( $conn, "SELECT SubmittedName, COUNT(*) SubmittedCnt FROM SubmittedItems GROUP BY SubmittedName ORDER BY 2 DESC");
+
+	if( $stmt === false ) {
+	     die( print_r( sqlsrv_errors(), true));
+	}
+	
+	while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+		 print "<tr><td>".$row['SubmittedName']."</td><td>".$row['SubmittedCnt']."</td></tr>\n";
+	}
+?>
+
+	    </table>
+        <br /><br /><br />
+        Running on <b><?php echo gethostname(); ?></b>
+		<hr>
+		Powered by <b>Azure App Service (Web App)</b>
+    </body>
+</html>
