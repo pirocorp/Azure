c8bfa4c06e0dd7bab27c2452f2d029b1f93d5739
Zdravko Zdravkov <zdr0@yahoo.com>
2022-04-18 19:09:32 +0200

    az204-cosmos

---
+++ 10. AZ-204 Develop solutions that use Azure Cosmos DB/az204-cosmos/CosmosService.cs
@@ -0,0 +1,50 @@
+﻿namespace az204_cosmos
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.Azure.Cosmos;
+
+    public class CosmosService
+    {
+        // The names of the database and container we will create
+        private const string ContainerId = "az204Container";
+        private const string DatabaseId = "az204Database";
+
+        // Replace <documentEndpoint> with the information created earlier
+        private const string EndpointUri = "https://zrz-cosmos.documents.azure.com:443/";
+
+        // Set variable to the Primary Key from earlier.
+        private const string PrimaryKey = "wJV3wDKqiztorii4pEle4EeOLnbifERYEata9ixcRtVMUq3WuA11U8ByUjYCrbt1RE5gpLy9RigIcBWvkFt2mQ==";
+
+        // The Cosmos client instance
+        private readonly CosmosClient cosmosClient;
+
+        // The database we will create
+        private Database database;
+
+        // The container we will create.
+        private Container container;
+
+        public CosmosService()
+        {
+            // Create a new instance of the Cosmos Client
+            this.cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+        }
+
+        public async Task<string> CreateDatabaseAsync(string databaseId = DatabaseId)
+        {
+            // Create a new database using the cosmosClient
+            this.database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+
+            return $"Created Database: {this.database.Id}\n";
+        }
+
+        public async Task<string> CreateContainerAsync(string containerId = ContainerId)
+        {
+            // Create a new container
+            this.container = await this.database.CreateContainerIfNotExistsAsync(containerId, "/LastName");
+
+            return $"Created Container: {this.container.Id}\n";
+        }
+    }
+}
---
+++ 10. AZ-204 Develop solutions that use Azure Cosmos DB/az204-cosmos/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace az204_cosmos
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Microsoft.Azure.Cosmos;
+
+    public class Program
+    {
+        public static async Task Main()
+        {
+            try
+            {
+                Console.WriteLine("Beginning operations...\n");
+                var cosmosService = new CosmosService();
+
+                var response = await cosmosService.CreateDatabaseAsync();
+                Console.WriteLine(response);
+
+                response = await cosmosService.CreateContainerAsync();
+                Console.WriteLine(response);
+            }
+            catch (CosmosException de)
+            {
+                var baseException = de.GetBaseException();
+                Console.WriteLine("{0} error occurred: {1}", de.StatusCode, de);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine("End of program, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
