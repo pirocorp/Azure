962b592ecb6355c6395224cab64464ed8a301aa6
Zdravko Zdravkov <pirocorp@abv.bg>
2022-05-17 22:51:04 +0200

    Updated solution (.net 6)

--- 19. Labs/06/Solution/OIDCClient/Controllers/HomeController.cs
+++ 19. Labs/06/Solution/OIDCClient/Controllers/HomeController.cs
@@ -1,40 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace OIDCClient.Controllers;
+
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using OIDCClient.Models;
 
-namespace OIDCClient.Controllers
+using Models;
+
+[Authorize]
+public class HomeController : Controller
 {
-    [Authorize]
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        this._logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return this.View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return this.View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [AllowAnonymous]
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [AllowAnonymous]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
     }
 }
--- 19. Labs/06/Solution/OIDCClient/Program.cs
+++ 19. Labs/06/Solution/OIDCClient/Program.cs
@@ -1,26 +1,84 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+namespace OIDCClient;
 
-namespace OIDCClient
+using System;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+public static class Program
 {
-    public class Program
+    private static IConfigurationSection? azureAdConfig;
+
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        ConfigureConfiguration(builder.Configuration);
+        ConfigureServices(builder.Services);
+
+        var app = builder.Build();
+
+        ConfigureMiddleware(app, app.Services);
+        ConfigureEndpoints(app, app.Services);
+        
+        app.Run();
+    }
+
+    private static void ConfigureConfiguration(IConfiguration configuration)
+    {
+        azureAdConfig = configuration.GetSection("AzureAd");
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        // Add services to the container.
+        services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApp(azureAdConfig ?? throw new InvalidOperationException("Missing AD configuration"));
+
+        services.AddControllersWithViews(options =>
         {
-            CreateHostBuilder(args).Build().Run();
+            var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+
+            options.Filters.Add(new AuthorizeFilter(policy));
+        });
+
+        services.AddRazorPages().AddMicrosoftIdentityUI();
+    }
+
+    private static void ConfigureMiddleware(WebApplication app, IServiceProvider services)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+    }
+
+    private static void ConfigureEndpoints(IEndpointRouteBuilder app, IServiceProvider services)
+    {
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.MapRazorPages();
     }
 }
