9da761d8ace13024b14baf31d17825fb0fe5cd30
Zdravko Zdravkov <zdr0@yahoo.com>
2022-05-20 17:27:37 +0200

    My version of Solution

---
+++ 19. Labs/10/Starter/MessagePublisher/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace MessagePublisher
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Azure.Messaging.ServiceBus;
+
+    public static class Program
+    {
+        private const string StorageConnectionString = "Endpoint=sb://sbnamespacezrz.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=h8JH0hPpHZGTTw+Qeu+UDaXZGWsRp4PZ4WEZa4imr4s=";
+
+        private const string QueueName = "messagequeue";
+
+        private const int NumOfMessages = 3;
+
+        private static readonly ServiceBusClient Client;
+
+        private static readonly ServiceBusSender Sender;
+
+        static Program()
+        {
+            Client = new ServiceBusClient(StorageConnectionString);
+            Sender = Client.CreateSender(QueueName);  
+        }
+
+        public static async Task Main()
+        {
+            using var messageBatch = await Sender.CreateMessageBatchAsync();
+
+            for (var i = 1; i <= NumOfMessages; i++)
+            {
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                await Sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {NumOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                await Sender.DisposeAsync();
+                await Client.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ 19. Labs/10/Starter/MessageReader/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace MessageReader
+{
+    using Azure.Messaging.ServiceBus;
+
+    public static class Program
+    {
+        private const string StorageConnectionString = "Endpoint=sb://sbnamespacezrz.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=h8JH0hPpHZGTTw+Qeu+UDaXZGWsRp4PZ4WEZa4imr4s=";
+        private const string QueueName = "messagequeue";
+        private static readonly ServiceBusClient Client;
+        private static readonly ServiceBusProcessor Processor;
+
+        static Program()
+        {
+            Client = new ServiceBusClient(StorageConnectionString);
+            Processor = Client.CreateProcessor(QueueName, new ServiceBusProcessorOptions());
+        }
+
+        public static async Task Main()
+        {
+            try
+            {
+                Processor.ProcessMessageAsync += MessageHandler;
+                Processor.ProcessErrorAsync += ErrorHandler;
+
+                await Processor.StartProcessingAsync();
+
+                Console.WriteLine("Wait for a minute and then press any key to end the processing");
+                Console.ReadKey();
+                Console.WriteLine("\nStopping the receiver...");
+
+                await Processor.StopProcessingAsync();
+
+                Console.WriteLine("Stopped receiving messages");
+            }
+            finally
+            {
+                await Processor.DisposeAsync();
+                await Client.DisposeAsync();
+            }
+        }
+
+        private static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            var body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        private static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+
+            return Task.CompletedTask;
+        }
+    }
+}
