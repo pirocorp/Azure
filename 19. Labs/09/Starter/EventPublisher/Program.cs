0bdca67b8f97f879495e2206c5911fb07c2ce4e3
Zdravko Zdravkov <zdr0@yahoo.com>
2022-05-20 17:26:54 +0200

    My version of Solution

---
+++ 19. Labs/09/Starter/EventPublisher/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace EventPublisher
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Azure;
+    using Azure.Messaging.EventGrid;
+
+    public static class Program
+    {
+        private const string TopicEndpoint = "https://hrtopiczrz.eastus-1.eventgrid.azure.net/api/events";
+
+        private const string TopicKey = "3nsDwGTK5GZArwSCEPxWD/FzZDmgWwrywHXoiZy0RrE=";
+
+        public static async Task Main()
+        {
+            var endpoint = new Uri(TopicEndpoint);
+            var credential = new AzureKeyCredential(TopicKey);
+
+            var client = new EventGridPublisherClient(endpoint, credential);
+
+            var firstEvent = new EventGridEvent(
+                subject: $"New Employee: Alba Sutton",
+                eventType: "Employees.Registration.New",
+                dataVersion: "1.0",
+                data: new
+                {
+                    FullName = "Alba Sutton",
+                    Address = "4567 Pine Avenue, Edison, WA 97202"
+                }
+            );
+
+            var secondEvent = new EventGridEvent(
+                subject: $"New Employee: Alexandre Doyon",
+                eventType: "Employees.Registration.New",
+                dataVersion: "1.0",
+                data: new
+                {
+                    FullName = "Alexandre Doyon",
+                    Address = "456 College Street, Bow, WA 98107"
+                }
+            );
+
+
+            await client.SendEventAsync(firstEvent);
+            Console.WriteLine("First event published");
+
+            await client.SendEventAsync(secondEvent);
+            Console.WriteLine("Second event published");
+        }
+    }
+}
